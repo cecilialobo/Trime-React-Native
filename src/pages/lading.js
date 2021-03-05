dc3d8ce05a0f437024d8e9c9d08d277a4636ad99
AcidDeliria <aleksandra.rzepecka@outlook.com>
2021-03-05 11:25:38 +0100

    added background img on home

--- src/pages/lading.js
+++ src/pages/lading.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { NativeRouter, Route, Link } from "react-router-native";
 
 import LogoLanding from '../../assets/images/logo-landing.svg'
+import LandingImg from '../../assets/images/landing-image.svg' 
 
-
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableHighlight, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView,TouchableHighlight, Button } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function Home() {
@@ -20,29 +20,40 @@ export default function Home() {
     };
 
     return (
-      <View style={styles.container}>
-        <LogoLanding style={styles.logo}/>
-          
-        <View style={styles.buttonContainer}>
-          <TouchableHighlight style={[styles.button, styles.signUpBtn]} onPress={signUp}>
-            <Link to="/sign-up-1"><Text style={styles.buttonText}>Sign Up</Text></Link>
-          </TouchableHighlight>
-          <TouchableHighlight style={[styles.button, styles.logInBtn]} onPress={logIn}><Text style={styles.buttonText}>Log In</Text></TouchableHighlight>
+        <View style={styles.container}>
+          <LandingImg style={styles.landingImg}/>
+            <LogoLanding style={styles.logo}/>
+              <View style={styles.buttonContainer}>
+                <TouchableHighlight style={[styles.button, styles.signUpBtn]} onPress={signUp}>
+                  <Link to="/sign-up-1"><Text style={styles.buttonText}>Sign Up</Text></Link>
+                </TouchableHighlight>
+                <TouchableHighlight style={[styles.button, styles.logInBtn]} onPress={logIn}><Text style={styles.buttonText}>Log In</Text></TouchableHighlight>
+              </View>
         </View>
-      </View>
     )
   }
  
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // takes up the entire screen
-    backgroundColor: '#082D4C',
+     flex: 1,// takes up the entire screen
+    // backgroundColor: '#082D4C',
     alignItems: 'center', //horizontal
-    justifyContent: 'space-between', 
+    justifyContent: 'space-between',
+
+  },
+  landingImg: {
+    flex: 1,
+    position: 'absolute',
+    zIndex: -10,
+    top: 0,
+    height: null,
+    width: null,
+    resizeMode: 'stretch',
   },
   logo: {
-    marginTop: 125
+    marginTop: 125,
+    zIndex: 10,
   },
   buttonContainer: {
     paddingBottom: 125,
