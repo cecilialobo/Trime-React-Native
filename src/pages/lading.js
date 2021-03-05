5f348da211f1e74a9e331f48c6a0a6584f920432
Emilia Frisell <emiliafrisell@gmail.com>
2021-03-05 19:32:29 +0100

    Update lading.js

--- src/pages/lading.js
+++ src/pages/lading.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { NativeRouter, Route, Link } from "react-router-native";
 
 import LogoLanding from '../../assets/images/logo-landing.svg'
+import SignIn from '../../assets/images/sign-in.svg'
 
 
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableHighlight, Button } from 'react-native';
+
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableHighlight, Button, TouchableOpacity } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function Home() {
@@ -20,16 +22,23 @@ export default function Home() {
     };
 
     return (
+      <>
       <View style={styles.container}>
         <LogoLanding style={styles.logo}/>
           
         <View style={styles.buttonContainer}>
-          <TouchableHighlight style={[styles.button, styles.signUpBtn]} onPress={signUp}>
-            <Link to="/sign-up-1"><Text style={styles.buttonText}>Sign Up</Text></Link>
+          <TouchableOpacity style={[styles.button, styles.signUpBtn]} onPress={signUp}>
+            <Link to="/sign-up-1" underlayColor="gray" style={ styles.link}>
+                <Text style={styles.buttonText}>Sign Up</Text></Link>
+          </TouchableOpacity>
+          <TouchableHighlight style={[styles.button, styles.logInBtn]} onPress={logIn}>
+            <Link to="/sign-up-3"><Text style={styles.buttonText}>Log In</Text></Link>
           </TouchableHighlight>
-          <TouchableHighlight style={[styles.button, styles.logInBtn]} onPress={logIn}><Text style={styles.buttonText}>Log In</Text></TouchableHighlight>
         </View>
+        <SignIn style={styles.bgImage}/>
+
       </View>
+      </>
     )
   }
  
@@ -37,28 +46,46 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1, // takes up the entire screen
-    backgroundColor: '#082D4C',
+    // backgroundColor: '#082D4C',
     alignItems: 'center', //horizontal
     justifyContent: 'space-between', 
+    zIndex: 100,
+    height: '100%'
+  },
+  bgImage: { 
+    flex: 1, 
+    width: 500, 
+    height: '100%', 
+    resizeMode:'stretch' ,
+    position: 'absolute',
+    top: 0,
+    zIndex: -1
   },
   logo: {
-    marginTop: 125
+    marginTop: 138
   },
   buttonContainer: {
-    paddingBottom: 125,
+    position: 'absolute',
+    top: 600,
   },
   button: {
     alignItems: 'center', //horizontal
     justifyContent: 'center', 
-
-    paddingVertical: 15,
-    paddingHorizontal: 40,
     margin: 10,
 
     borderRadius: 40,
 
     width: 260,
     height: 73,
+    opacity: 0.8
+  },
+  link: {
+    width:'100%', 
+    height: '100%', 
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    borderRadius: 40
   },
   buttonText: {
     fontSize: 30,
