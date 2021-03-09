6720f3fe4588ea02ef8b716f672fcaf7e44f1610
Cecilia Lobo <cecilialobo92@gmail.com>
2021-03-09 13:20:47 +0100

    Remove unused libraries

--- src/components/searchOptions.js
+++ src/components/searchOptions.js
@@ -1,6 +1,4 @@
-
 import React, { useState } from 'react';
-import { TextInput, SectionList } from 'react-native';
 // import DatePicker from 'react-native-date-picker';
 import { StyleSheet, Text, View, Image, SafeAreaView, TouchableHighlight, Button } from 'react-native';
 
