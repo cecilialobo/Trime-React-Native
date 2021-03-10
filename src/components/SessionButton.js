0a89b367205f69c50a3a51a0c33fcaa39fb04e41
Cecilia Lobo <cecilialobo92@gmail.com>
2021-03-10 17:05:33 +0100

    Update SessionButtons logic

--- src/components/SessionButton.js
+++ src/components/SessionButton.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableHighlight } from 'react-native';
 
-export default function SessionButton( {sessionType} ) {
+export default function SessionButton( {sessionType, isPressed} ) {
+
+    const [ isPress, setIsPress ] = useState(false);
+
+    const touchProps = {
+        activeOpacity: 1,
+        underlayColor: '#0BD8A7',                            
+        style: isPress ? styles.btnPress : styles.btnNormal, 
+        onPress: () => {
+            isPress 
+            ? setIsPress(false)  
+            : setIsPress(true) & console.log(`Session type: ${sessionType}`)
+        },               
+      };
 
     return (
-            <TouchableHighlight 
-            style={styles.sessionButton} 
-            underlayColor="#0BD8A7" 
-            onPress={() => console.log(`Session type: ${sessionType}`)}>
-                <Text style={{color: '#fff', fontSize: 12}}>{sessionType}</Text>
-            </TouchableHighlight>
+        <TouchableHighlight {...touchProps}>
+            <Text style={{color: '#fff', fontSize: 12}}>{sessionType}</Text>
+        </TouchableHighlight>
     )
 }
 
 const styles = StyleSheet.create({
-    sessionButton: {
+    btnPress: {
+        backgroundColor: '#0BD8A7',
+        height: 33,
+        width: 97,
+        borderRadius: 40,
+        alignItems: 'center',
+        justifyContent: 'center', 
+    },
+    btnNormal: {
         backgroundColor: '#05668D',
         height: 33,
         width: 97,
