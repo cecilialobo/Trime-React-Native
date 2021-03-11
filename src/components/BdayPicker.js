bd91b37c8e094194b6b08f1ec6228b2f6a87b059
Emilia Frisell <emiliafrisell@gmail.com>
2021-03-11 22:03:03 +0100

    start date 1990

--- src/components/BdayPicker.js
+++ src/components/BdayPicker.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Button, TouchableOpacity, Text, Dimensions } from 'react-native';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
+import moment from 'moment';
 
 const { width, height} = Dimensions.get('window');
 
@@ -19,13 +20,27 @@ export default function BdayPicker ( { day, setDay, month, setMonth, year, setYe
   
     const handleConfirm = (output) => {
         let date, month
+
         output.getDate() < 10 
             ? date = '0' + output.getDate()
             : date = output.getDate();
 
-        output.getMonth() < 10
-            ? month = '0' + output.getMonth()
-            : month = output.getMonth()
+        // output.getMonth() < 10
+        //     ? month = '0' + (output.getMonth() + 1)
+        //     : month = (output.getMonth() + 1)
+
+        if (output.getMonth() + 1 === 1 ) month = 'January'
+        else if (output.getMonth()+ 1 === 2 ) month = 'February'
+        else if (output.getMonth() + 1 === 3 ) month = 'March'
+        else if (output.getMonth() + 1 === 4 ) month = 'April'
+        else if (output.getMonth() + 1 === 5 ) month = 'May'
+        else if (output.getMonth() + 1 === 6 ) month = 'June'
+        else if (output.getMonth() + 1 === 7 ) month = 'July'
+        else if (output.getMonth() + 1 === 8 ) month = 'August'
+        else if (output.getMonth() + 1 === 9 ) month = 'September'
+        else if (output.getMonth() + 1 === 10 ) month = 'Oktober'
+        else if (output.getMonth() + 1 === 11 ) month = 'November'
+        else if (output.getMonth() + 1 === 12 ) month = 'December'
 
         var year = output.getFullYear();
         setDay(date)
@@ -42,10 +57,10 @@ export default function BdayPicker ( { day, setDay, month, setMonth, year, setYe
             <Text style={styles.label}>Birth Day</Text>
             <View style={styles.inputContainer}>
                 <TouchableOpacity style={styles.inputStyle} onPress={showDatePicker}>
-                    <Text style={styles.text}>{day}</Text>
+                    <Text style={styles.text}>{month}</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.inputStyle} onPress={showDatePicker}>
-                    <Text style={styles.text}>{month}</Text>
+                    <Text style={styles.text}>{day}</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.inputStyle} onPress={showDatePicker}>
                     <Text style={styles.text}>{year}</Text>
@@ -55,7 +70,7 @@ export default function BdayPicker ( { day, setDay, month, setMonth, year, setYe
                     mode='date'
                     onConfirm={handleConfirm}
                     onCancel={hideDatePicker}
-                    // date={01, 01, 1990}
+                    date={'1990-01-01T20:59:21.239Z'}
                 />
             </View>
         </View>
